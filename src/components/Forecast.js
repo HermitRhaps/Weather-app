02e31e7c4255dabb60b048f70706daeb5aa601ad
Dmitry Nemov <hermit.requiem@gmail.com>
2021-03-14 21:09:02 +0300

    Finished with this project, add some key validation and delete old code.

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -18,7 +18,7 @@ import Cities from "/Users/hermitrhaps/Documents/development/Weather-app/node_mo
 import CountriesData from "/Users/hermitrhaps/Documents/development/Weather-app/node_modules/countries-list/dist/countries.emoji.json";
 const API_URL = "https://api.openweathermap.org/data/2.5/";
 const REQUEST_TYPE = "onecall?";
-const USR_KEY = "&appid=83c6ba4dd07d83514536821a8a51d6d5";
+const USR_KEY = "";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -52,7 +52,7 @@ const Forecast = () => {
   const [country, setCountry] = useState({ i: "", name: "", flag: "" });
   const [city, setCity] = useState("");
   const [forecast, setForecast] = useState();
-
+  const [keyError, setKeyError] = useState(false);
   const classes = useStyles();
   const countryValidate = (e) => {
     Object.entries(CountriesData)
@@ -67,30 +67,33 @@ const Forecast = () => {
   };
   const search = (e) => {
     e.preventDefault();
-    Object.entries(Cities)
-      .filter(([index, info]) => info.name === city)
-      .forEach(([index, info]) => {
-        fetch(
-          API_URL +
-            REQUEST_TYPE +
-            "lat=" +
-            info.lat +
-            "&lon=" +
-            info.lng +
-            "&units=metric" +
-            USR_KEY
-        )
-          .then((response) => {
-            if (!response.ok) {
-              throw Error(response.statusText);
-            }
-            return response.json();
-          })
-          .then((data) => {
-            setForecast(data);
-          })
-          .catch((error) => console.log(error));
-      });
+    setForecast();
+    return USR_KEY
+      ? Object.entries(Cities)
+          .filter(([index, info]) => info.name === city)
+          .forEach(([index, info]) => {
+            fetch(
+              API_URL +
+                REQUEST_TYPE +
+                "lat=" +
+                info.lat +
+                "&lon=" +
+                info.lng +
+                "&units=metric&appid=" +
+                USR_KEY
+            )
+              .then((response) => {
+                if (!response.ok) {
+                  throw Error(response.statusText);
+                }
+                return response.json();
+              })
+              .then((data) => {
+                setForecast(data);
+              })
+              .catch((error) => console.log(error));
+          }) || setKeyError(false)
+      : setKeyError(true);
   };
   return (
     <Container maxWidth="lg" className={classes.container}>
@@ -168,7 +171,7 @@ const Forecast = () => {
             </form>
           </Paper>
         </Grid>
-        {forecast ? (
+        {forecast && !keyError ? (
           <>
             <Grid item xs={12}>
               <Paper className={classes.paper} elevation={3}>
@@ -194,82 +197,14 @@ const Forecast = () => {
             </Grid>
           </>
         ) : (
-          false
+          <Grid item xs={12}>
+            <Paper className={classes.paper} elevation={3}>
+              <Typography variant="h6">There no API key, check it!</Typography>
+            </Paper>
+          </Grid>
         )}
       </Grid>
     </Container>
   );
 };
-// export default class Forecast extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       timezone: "",
-//       current: {},
-//       hourly: {},
-//       daily: {},
-//       searched: false,
-//       error: null,
-//       coords: { lat: null, lon: null },
-//     };
-//   }
-//   componentDidMount() {
-//     navigator.geolocation.getCurrentPosition((position) => {
-// fetch(
-//   API_URL +
-//     REQUEST_TYPE +
-//     "lat=" +
-//     position.coords.latitude +
-//     "&lon=" +
-//     position.coords.longitude +
-//     "&units=metric" +
-//     USR_KEY
-// )
-//   .then((response) => {
-//     if (!response.ok) {
-//       throw Error(response.statusText);
-//     }
-//     return response.json();
-//   })
-//   .then((data) => {
-//     this.setState({
-//       timezone: data.timezone,
-//       current: data.current,
-//       hourly: data.hourly,
-//       daily: data.daily,
-//       searched: true,
-//       coords: {
-//         lat: data.lat,
-//         lon: data.lon,
-//       },
-//     });
-//   })
-//   .catch((error) => this.setState({ error, searched: false }));
-//     });
-//   }
-//   render() {
-//     return (
-//       <section>
-//         {this.state.searched ? (
-//           <div className="container">
-//             <div className="card shadow p-3 mb-5 bg-white rounded">
-// <CurrentForecast
-//   timezone={this.state.timezone}
-//   current={this.state.current}
-// />
-//             </div>
-//             <div className="card shadow p-3 mb-5 bg-white rounded">
-// <HourlyForecast hourly={this.state.hourly} />
-//             </div>
-//             <div className="card shadow p-3 mb-5 bg-white rounded">
-//               <DailyForecast daily={this.state.daily} />
-//             </div>
-//           </div>
-//         ) : (
-//           <></>
-//         )}
-//       </section>
-//     );
-//   }
-// }
 export default Forecast;
